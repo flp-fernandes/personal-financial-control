927705730e615561a9957974daebd6aeaf9d61fd
Felipe Fernandes <hey.felipao@gmail.com>
2024-09-02 14:30:43 -0300

    added authentication service login

--- src/authentication/authentication.service.ts
+++ src/authentication/authentication.service.ts
@@ -1,4 +1,8 @@
 import { Injectable } from '@nestjs/common';
 
 @Injectable()
-export class AuthenticationService {}
+export class AuthenticationService {
+  async login(loginDto: any) {
+    return 'This action logs a user in';
+  }
+}
